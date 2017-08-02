dd3e1fd26b0811e3b8835a192bacc0e02873168e
Berlin Sky <pedroyoung8989@gmail.com>
2017-08-02 12:16:28 -0700

    updates

--- app/ts/app/blogDetail/blog-detail.component.ts
+++ app/ts/app/blogDetail/blog-detail.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject'
 
 @Component({
   templateUrl: './blog-detail.component.pug',
-   styleUrls: ['blog-detail.component.scss']
+  styleUrls: ['blog-detail.component.scss']
 })
 export class BlogDetailComponent {
-  timer = Observable
-    .interval(1000)
-    .map(() => new Date());
+  private subject$ = new Subject<any>();
+  timer: any;
+
+  // timer = Observable
+  //   .interval(1000)
+  //   .map(() => new Date());
 
   constructor() {
     // this.timer.subscribe(console.log.bind(console));
+    this.timer = this.subject$.map(() => new Date());
   }
 }
