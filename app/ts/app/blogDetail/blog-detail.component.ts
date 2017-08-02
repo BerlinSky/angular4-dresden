23d5bb95385be2dee86cb29c93f3b99bd9792a42
Berlin Sky <pedroyoung8989@gmail.com>
2017-08-01 22:48:11 -0700

    updates

--- app/ts/app/blogDetail/blog-detail.component.ts
+++ app/ts/app/blogDetail/blog-detail.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   templateUrl: './blog-detail.component.pug',
    styleUrls: ['blog-detail.component.scss']
 })
 export class BlogDetailComponent {
+  timer = Observable
+    .interval(1000)
+    .map(() => new Date());
+
+  constructor() {
+    // this.timer.subscribe(console.log.bind(console));
+  }
 }
--- app/ts/app/blogDetail/blog-detail.module.ts
+++ app/ts/app/blogDetail/blog-detail.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { BlogDetailComponent } from './blog-detail.component';
 import  blogDetailRoutes  from './blog-detail.routes';
 
 @NgModule({
-  imports: [blogDetailRoutes],
+  imports: [CommonModule, blogDetailRoutes],
   declarations: [BlogDetailComponent],
   exports: [BlogDetailComponent]
 })
