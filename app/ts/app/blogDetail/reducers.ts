1463202c7523c5370755d558409c6429a0352418
Berlin Sky <pedroyoung8989@gmail.com>
2017-08-07 18:09:42 -0700

    updates

--- app/ts/app/blogDetail/reducers.ts
+++ app/ts/app/blogDetail/reducers.ts
@@ -1,3 +1,15 @@
-export const timer = (state = new Date()) => {
+export const timer = (state = new Date(), { type }: any) => {
+  const date = new Date(state.getTime());
+
+  switch (type) {
+    case 'second':
+      date.setSeconds(date.getSeconds() + 5);
+      return date;
+
+    case 'hour':
+      date.setHours(date.getHours() + 1);
+      return date;
+  }
+
   return state
 }
