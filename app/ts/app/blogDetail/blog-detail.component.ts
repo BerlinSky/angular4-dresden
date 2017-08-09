93146c1a2e62ab667a48ec134f5c0ea8be8e5d77
Berlin Sky <pedroyoung8989@gmail.com>
2017-08-08 17:01:25 -0700

    Updates

--- app/ts/app/blogDetail/blog-detail.component.ts
+++ app/ts/app/blogDetail/blog-detail.component.ts
@@ -10,18 +10,24 @@ import { HOUR, SECOND } from './reducers'
   styleUrls: ['blog-detail.component.scss']
 })
 export class BlogDetailComponent {
-  private subject$ = new Subject<any>();
+  hours$ = new Subject<any>()
+    .map((value) => ({ type: HOUR, payload: parseInt(value, 10)}))
+    // .mapTo({ type: HOUR, payload: 1 })
+  seconds$ = Observable.interval(3000)
+    .mapTo({ type: SECOND, payload: 5 })
+
   timer: any;
 
   constructor(private store: Store<any>) {
     this.timer = store.select('timer');
 
     Observable.merge(
-      this.subject$.mapTo({ type: HOUR, payload: 1 }),
-      Observable.interval(3000).mapTo({ type: SECOND, payload: 5 })
+      this.hours$,
+      this.seconds$
     )
-    .subscribe((action) => {
-      this.store.dispatch(action)
-    })
+    .subscribe(store.dispatch.bind(store))
+    // .subscribe((action) => {
+    //   this.store.dispatch(action)
+    // })
   }
 }
